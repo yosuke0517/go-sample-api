eb21e464a2401b93aa11a0832461ab63ed8a27e9
yosuke0517 <x.clover.x007@gmail.com>
2020-02-16 23:09:17 +0900

    routerまで

--- main.go
+++ main.go
@@ -2,12 +2,23 @@
 
     import (
         "net/http"
-        "./routes"
+        "app/routes"
         "github.com/labstack/echo"
+        "github.com/labstack/echo/middleware"
+        "github.com/sirupsen/logrus"
     )
+
+    func init() {
+         logrus.SetLevel(logrus.DebugLevel)
+         logrus.SetFormatter(&logrus.JSONFormatter{})
+    }
 
     func main() {
         e := echo.New()
+
+        //Middlewares
+        e.Use(middleware.Logger())
+
         // Routes
         routes.Init(e)
         e.GET("/", func(c echo.Context) error {
--- routes/api.go
+++ routes/api.go
@@ -1,12 +1,14 @@
 package routes
 
 import (
-	"github.com/app/web/api"
+	// "github.com/app/web/api"
+	"app/web/api"
 	"github.com/labstack/echo"
 	)
 
 	func Init(e *echo.Echo) {
-		g := e.Group("/api"){
+		g := e.Group("/api") 
+		{
 			g.GET("/popular", api.FetchMostPopularVideos())
 		}
 	}
